722913fbbe65cc7fcf037356639274503509bd66
tnwjd2050 <tnwjd2050@naver.com>
2020-08-05 22:40:56 +0900

    캐러셀 슬라이더 만들기 최초 작업 추가

---
+++ 박수정/scripts/carousel.js
@@ -0,0 +1,50 @@
+class slider {
+    constructor(selector, option) {
+        this.selector = document.querySelector(selector);
+
+        if (option) {
+
+        }
+
+        this.CONTAINERNAME = 'slider__container';
+        this.WRAPPERRNAME = 'slider__wrapper';
+        this.ITEM = '.slider__item';
+    }
+    makeHTML() {
+        const wrapper = document.createElement('div');
+        const slideItemHTML = this.selector.innerHTML;
+
+        this.selector.classList.add(this.CONTAINERNAME);
+        this.selector.innerHTML = '';
+        this.selector.appendChild(wrapper);
+
+        wrapper.innerHTML = slideItemHTML;
+        wrapper.className = this.WRAPPERRNAME;
+    }
+    setWidth() {
+        const items = document.querySelectorAll(this.ITEM);
+        const container = document.querySelector(`.${this.CONTAINERNAME}`);
+
+        let getWidth = items[0].offsetWidth;
+
+        const assignGetWidth = function (idx) {
+            getWidth = items[idx].offsetWidth;
+        };
+
+        for (let i = 0; i < items.length; i++) {
+            if (getWidth < items[i].offsetWidth) {
+                assignGetWidth(i);
+            }
+        }
+
+        for (let i = 0; i < items.length; i++) {
+            items[i].style.width = `${getWidth}px`;
+        }
+
+        container.style.width = `${getWidth}px`;
+    }
+    init() {
+        this.makeHTML();
+        this.setWidth();
+    }
+}
---
+++ 박수정/scripts/init.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const option = {
+        coverImg: true
+    }
+    const sliderTest = new slider('.user-slider');
+    // const sliderTest = new slider('.user-slider', option);
+    sliderTest.init();
+}
